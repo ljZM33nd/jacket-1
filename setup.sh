12aad2c1b3187c1b7e639bfbc3143197ea47dcac
whysay88 <17911212@qq.com>
2016-05-17 16:50:48 +0800

    code format
    Change-Id: I9c39ac12d85618955c000d4cc811d0845214601b

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 ###########################################
 
 
-LOG_DIR=/var/log/fusionsphere/component/jacket 
+LOG_DIR=/var/log/fusionsphere/component/jacket
 LOG_FILE=${LOG_DIR}/install.log
 
 #source code file directory
@@ -17,53 +17,53 @@ TIME_CMD=`date '+%Y-%m-%d %H:%M:%S'`
 copy_files_to_dir()
 {
     #copy vcloud source code
-	if [ -d ${CODE_DIR}/nova/virt/jacket ]; then
-       echo rm -rf ${CODE_DIR}/nova/virt/jacket
-	   rm -rf ${CODE_DIR}/nova/virt/jacket
-	fi
+    if [ -d ${CODE_DIR}/nova/virt/jacket ]; then
+        echo rm -rf ${CODE_DIR}/nova/virt/jacket
+        rm -rf ${CODE_DIR}/nova/virt/jacket
+    fi
     echo cp -r ./jacket/nova/virt/jacket ${CODE_DIR}/nova/virt/
     cp -r ./jacket/nova/virt/jacket ${CODE_DIR}/nova/virt/
-    
-	if [ -d ${CODE_DIR}/cinder/volume/drivers/jacket ]; then
-       echo rm -rf ${CODE_DIR}/cinder/volume/drivers/jacket
-	   rm -rf ${CODE_DIR}/cinder/volume/drivers/jacket
-	fi
+
+    if [ -d ${CODE_DIR}/cinder/volume/drivers/jacket ]; then
+        echo rm -rf ${CODE_DIR}/cinder/volume/drivers/jacket
+        rm -rf ${CODE_DIR}/cinder/volume/drivers/jacket
+    fi
     echo cp -r ./jacket/cinder/volume/drivers/jacket ${CODE_DIR}/cinder/volume/drivers
     cp -r ./jacket/cinder/volume/drivers/jacket ${CODE_DIR}/cinder/volume/drivers
-    
+
     #copy aws source code
-	if [ -d ${CODE_DIR}/nova/virt/aws ]; then
-       echo rm -rf ${CODE_DIR}/nova/virt/aws
-	   rm -rf ${CODE_DIR}/nova/virt/aws
-	fi
+    if [ -d ${CODE_DIR}/nova/virt/aws ]; then
+        echo rm -rf ${CODE_DIR}/nova/virt/aws
+        rm -rf ${CODE_DIR}/nova/virt/aws
+    fi
     echo cp -r ./jacket/nova/virt/aws ${CODE_DIR}/nova/virt/
     cp -r ./jacket/nova/virt/aws ${CODE_DIR}/nova/virt/
-    
-	if [ -d ${CODE_DIR}/cinder/volume/drivers/ec2 ]; then
-       echo rm -rf ${CODE_DIR}/cinder/volume/drivers/ec2
-	   rm -rf ${CODE_DIR}/cinder/volume/drivers/ec2
-	fi
-    echo cp -r ./jacket/cinder/volume/drivers/ec2 ${CODE_DIR}/cinder/volume/drivers 
+
+    if [ -d ${CODE_DIR}/cinder/volume/drivers/ec2 ]; then
+        echo rm -rf ${CODE_DIR}/cinder/volume/drivers/ec2
+        rm -rf ${CODE_DIR}/cinder/volume/drivers/ec2
+    fi
+    echo cp -r ./jacket/cinder/volume/drivers/ec2 ${CODE_DIR}/cinder/volume/drivers
     cp -r ./jacket/cinder/volume/drivers/ec2 ${CODE_DIR}/cinder/volume/drivers
-    
+
     echo ""
 }
 
 clear_files()
-{ 
+{
     #remove source code files
     echo rm -rf ${CODE_DIR}/nova/virt/jacket
     rm -rf ${CODE_DIR}/nova/virt/jacket
-    
+
     echo rm -rf ${CODE_DIR}/cinder/volume/drivers/jacket
     rm -rf ${CODE_DIR}/cinder/volume/drivers/jacket
-    
-    echo rm -rf ${CODE_DIR}/nova/virt/aws    
+
+    echo rm -rf ${CODE_DIR}/nova/virt/aws
     rm -rf ${CODE_DIR}/nova/virt/aws
-    
+
     echo rm -rf ${CODE_DIR}/cinder/volume/drivers/ec2
     rm -rf ${CODE_DIR}/cinder/volume/drivers/ec2
-    
+
     echo ""
 }
 
@@ -73,19 +73,19 @@ chown -R openstack:openstack ${LOG_DIR}
 
 init() {
 :
-}   
-   
+}
+
 start() {
     echo ""
     echo "...cps host-template-instance-operate --action start --service nova nova-compute..."
     cps host-template-instance-operate --action start --service nova nova-compute
-    
+
     echo ""
-    echo "...cps host-template-instance-operate --action start --service cinder cinder-volume..."    
+    echo "...cps host-template-instance-operate --action start --service cinder cinder-volume..."
     cps host-template-instance-operate --action start --service cinder cinder-volume
-    
+
     echo ""
-    echo "...cps host-template-instance-operate --action start --service cinder cinder-backup..."    
+    echo "...cps host-template-instance-operate --action start --service cinder cinder-backup..."
     cps host-template-instance-operate --action start --service cinder cinder-backup
 }
 
@@ -94,27 +94,27 @@ stop() {
     echo ""
     echo "...cps host-template-instance-operate --action stop --service nova nova-compute..."
     cps host-template-instance-operate --action stop --service nova nova-compute
-    
+
     echo ""
-    echo "...host-template-instance-operate --action stop --service cinder cinder-volume..."    
+    echo "...host-template-instance-operate --action stop --service cinder cinder-volume..."
     cps host-template-instance-operate --action stop --service cinder cinder-volume
-    
+
     echo ""
-    echo "...cps host-template-instance-operate --action stop --service cinder cinder-backup..."    
+    echo "...cps host-template-instance-operate --action stop --service cinder cinder-backup..."
     cps host-template-instance-operate --action stop --service cinder cinder-backup
 }
 restart() {
-	stop; sleep 1; start;
+    stop; sleep 1; start;
 }
 
 install() {
-     echo "install jacket..."     
+     echo "install jacket..."
      init
-   
+
      #copy source_code
      echo "...copy_files_to_dir..."
      copy_files_to_dir
-     
+
      #start service
      echo restart service
      restart
@@ -126,12 +126,12 @@ uninstall() {
      #stop service
      echo stop service
      stop
-     
+
      #clear source_code bin config_file
      echo ""
      echo "...clear_files..."
      clear_files
-     
+
      echo ""
 }
 
